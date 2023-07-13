7d6a1087965e40cb03e2aba8aac54248aa18e7db
Erin Cain <ecain@flowwest.com>
2023-07-13 14:53:30 -0700

    adds plot to explore results file

--- data-raw/explore-results.R
+++ data-raw/explore-results.R
@@ -1,12 +1,31 @@
 library(springRunDSM)
 library(tidyverse)
 r2r_seeds <- springRunDSM::spring_run_model(scenario = NULL, mode = "seed",
-                                            seeds = NULL, ..params = springRunDSM::r_to_r_baseline_params, stochastic = FALSE)
+                                            seeds = NULL, ..params = springRunDSM::r_to_r_baseline_params)
 
 r2r_model_results <- springRunDSM::spring_run_model(mode = "simulate", ..params = springRunDSM::r_to_r_baseline_params,
                                                 seeds = r2r_seeds)
 r2r_model_results$spawners
 r2r_model_results$phos
+
+spawn <- dplyr::as_tibble(r2r_model_results$spawners) |>
+  dplyr::mutate(location = fallRunDSM::watershed_labels) |>
+  pivot_longer(cols = c(`1`:`20`), values_to = 'spawners', names_to = "year") %>%
+  group_by(year, location) |>
+  summarize(total_spawners = sum(spawners)) |>
+  filter(location != "Feather River") |>
+  mutate(year = as.numeric(year)) %>%
+  ggplot(aes(year, total_spawners, color = location)) +
+  geom_line() +
+  theme_minimal() +
+  labs(y = "Spawners",
+       x = "Year") +
+  scale_y_continuous(labels = scales::comma) +
+  scale_x_continuous(breaks = 1:20) +
+  theme(text = element_text(size = 20))
+
+spawn
+
 
 plot_total_spawners <- function(model_results,
                                 result_type = c("Total Spawners", "Total Natural Spawners")) {
