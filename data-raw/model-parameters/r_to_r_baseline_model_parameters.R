e9d8636083c3644a27b209b547130d5fa8b1e7d0
liz-fw <lstebbins@flowwest.com>
2025-01-30 09:15:01 -0700

    puts hypothesis weights to default to 1 and 0s for time being

--- data-raw/model-parameters/r_to_r_baseline_model_parameters.R
+++ data-raw/model-parameters/r_to_r_baseline_model_parameters.R
@@ -260,7 +260,7 @@ r_to_r_baseline_params <- list(
   flows_apr_may = DSMflow::hatchery_apr_may_flows$biop_itp_2018_2019,
   
   # multi route
-  movement_hypo_weights = rep(1/8, 8),
+  movement_hypo_weights = c(1, rep(0, 7)), #rep(1/8, 8),
   ..habitat_capacity = 5,
   ..floodplain_capacity = 5
 )
